ef9077aaa9c449a91486cf3bef0a9f90a5dd47d1
biradarsapna <118991570+biradarsapna@users.noreply.github.com>
2026-05-03 08:54:25 +0000

    commit

--- frontend-next/.next/static/chunks/webpack.js
+++ frontend-next/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ddf29af5ab793c9e"; }
+/******/ 		__webpack_require__.h = function() { return "5787bb179f518a98"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
